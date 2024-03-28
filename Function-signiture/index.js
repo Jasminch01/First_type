eb8cf6a14d98c806544d21f53e40d93a02349e41
jasmin <chjasmin27@gmail.com>
2024-03-28 17:23:16 +0600

    example

--- Function-signiture/index.js
+++ Function-signiture/index.js
@@ -1,2 +1,20 @@
 //function signiture is a blueprint of a function
 var myfunc;
+myfunc = function (x, y) {
+    return x + y;
+};
+// console.log(myfunc( 4, 2,))
+var subs;
+subs = function (a, b) {
+    console.log("this is ".concat(a, " and ").concat(b));
+};
+var add;
+add = function (a, b, c) {
+    if (c === "add") {
+        return a + b;
+    }
+    else {
+        return a - b;
+    }
+};
+console.log(add(2, 5, ""));
--- Function-signiture/index.ts
+++ Function-signiture/index.ts
@@ -1,2 +1,28 @@
 //function signiture is a blueprint of a function
-let myfunc : (x: string , y: number) => number;
+let myfunc : (x: number , y: number) => number;
+
+myfunc = (x: number, y:number ) => {
+     return x+y;
+}
+
+// console.log(myfunc( 4, 2,))
+
+let subs : (a: string | number , b : string | number) => void;
+
+subs = (a: string | number , b : string | number) => {
+    console.log(`this is ${a} and ${b}`)
+}
+
+let add : (a: number, b:number, c: string) => number;
+
+add = (a, b, c) => {
+    if (c === "add") {
+        return a + b
+    }else{
+        return a-b;
+    }
+}
+
+console.log(add(2, 5, ""))
+
+
